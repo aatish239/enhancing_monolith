63dc71cdeef51288beb6ec28dde455c95534ff9f
aatish239 <40890266+aatish239@users.noreply.github.com>
2025-01-31 21:28:16 +0530

    Data preparation stage- movie dataset
    creating movie and movies dataset.

---
+++ data_preparation/process_imdb_movies_data.py
@@ -0,0 +1,21 @@
+# import statements
+import pandas as pd
+
+# loading the databases from non commercial imdb website
+ratings = pd.read_csv("../raw_datasets/ratings.tsv", sep='\t')
+crew = pd.read_csv('../raw_datasets/crew.tsv', sep='\t')
+episode = pd.read_csv('../raw_datasets/episode.tsv', sep='\t')
+basics = pd.read_csv('../raw_datasets/basics.tsv', sep='\t')
+title_basics = pd.read_csv('../raw_datasets/title_basics.tsv', sep='\t', low_memory=False)
+
+# merge the datasets to create unified movies dataset and movies people dataset
+inter1 = pd.merge(ratings, crew, on='tconst', how='left')
+movies_df = pd.merge(inter1, title_basics, on='tconst', how='left')
+
+# export the datasets to the csv files
+movies_df.to_csv('../datasets/movies.csv')
+basics.to_csv('../datasets/movie_people.csv')
+
+# print statements
+print("Movies dataset head", movies_df.head())
+print("Movies people head", basics.head())
