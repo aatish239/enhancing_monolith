a36d5a73ad0c3de4e36a6ba91e6473e33fa2ac8b
aatish239 <40890266+aatish239@users.noreply.github.com>
2025-02-04 21:06:58 +0530

    filter movies dataset with only movies having reviews

--- data_preparation/process_imdb_movies_data.py
+++ data_preparation/process_imdb_movies_data.py
@@ -14,9 +14,20 @@ movies_df = pd.merge(inter1, title_basics, on='tconst', how='left')
 
 # export the datasets to the csv files
 ratings.to_csv('../raw_datasets/movie_ratings.csv') # will be used for web scraping
-movies_df.to_csv('../datasets/movies.csv')
+movies_df.to_csv('../raw_datasets/movies.csv')
 basics.to_csv('../datasets/movie_people.csv')
 
 # print statements
 print("Movies dataset head", movies_df.head())
 print("Movies people head", basics.head())
+
+movies_df = pd.read_csv('../raw_datasets/movies.csv', index_col=0)
+users_df = pd.read_csv('../raw_datasets/moview_user_reviews.csv', names=['user_id', 'movie_id', 'user_rating',
+                                                                       'user_review'])
+movies_df_1 = movies_df[movies_df['tconst'].isin(users_df.movie_id)]
+
+users_df = pd.read_csv('../raw_datasets/moview_user_reviews_20k.csv', names=['user_id', 'movie_id', 'user_rating',
+                                                                    'user_review'])
+movies_df_2 = movies_df[movies_df['tconst'].isin(users_df.movie_id)]
+combined_df = pd.concat([movies_df_1, movies_df_2]).drop_duplicates(subset='tconst')
+combined_df.to_csv('../datasets/movies.csv')
