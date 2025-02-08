908236cdcc756a0ec66a46fff3551d74907dd1e7
aatish239 <40890266+aatish239@users.noreply.github.com>
2025-02-08 21:00:35 +0530

    model training

--- model_training/model_training.py
+++ model_training/model_training.py
@@ -1,47 +1,14 @@
-# Data Splitting
-from sklearn.model_selection import train_test_split
-import pandas as pd
-from transformers import BertTokenizer, BertModel
-import torch
-
-users_data = pd.read_csv('../processed_data/users_data.csv', index_col=0)
-print(users_data.head())
-train_data, temp_data = train_test_split(users_data, test_size=0.3, random_state=42)
-val_data, test_data = train_test_split(temp_data, test_size=0.5, random_state=42)
-
-# BERT Embeddings
-tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-bert_model = BertModel.from_pretrained('bert-base-uncased')
-
-def get_bert_embeddings(reviews):
-    inputs = tokenizer(reviews, return_tensors='pt', padding=True, truncation=True)
-    with torch.no_grad():
-        outputs = bert_model(**inputs)
-    cls_embeddings = outputs.last_hidden_state[:, 0, :]  # Shape: [batch_size, 768]
-    # Average if multiple embeddings exist
-    if cls_embeddings.shape[0] > 1:
-        return torch.mean(cls_embeddings, dim=0)  # Shape: [768]
-    else:
-        return cls_embeddings.squeeze(0)  # Shape: [768]
-
-
-# train_data['bert_embeddings'] = train_data['user_reviews'].apply(get_bert_embeddings)
-
-# Iterating over rows
-train_data_with_embeddings = pd.DataFrame(columns=train_data.columns)
-train_data_with_embeddings.to_csv('../processed_data/training_users_data.csv', header=True)
-batch_data = []
-batch_size = 10
-for index, row in train_data.iterrows():
-    user_reviews = row['user_reviews']
-    embedding = get_bert_embeddings(user_reviews)
-    row_copy = row.copy()
-    row_copy['user_reviews'] = embedding
-    print(f"Index: {index}, row {row_copy}")
-    batch_data.append(row_copy)
-    if (index + 1) % batch_size == 0:
-        batch_df = pd.DataFrame(batch_data)
-        batch_df.to_csv('../processed_data/training_users_data.csv', mode='a', header=False)
-        batch_data = []  # Clear memory
-   
-    
+# Feature Engineering:
+#
+# Process genres using one-hot encoding or embeddings.
+# Normalize ratings and runtime.
+# Use user_reviews as user preference vectors.
+# Model Design:
+#
+# Neural Network with:
+# User Embeddings: user_id, user_reviews
+# Movie Embeddings: Genres, directors, etc.
+# Auxiliary Tasks: (e.g., rating prediction)
+# Training & Predictions:
+#
+# Recommend 2 movies per user (top-N recommendation).
